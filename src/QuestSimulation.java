62b90d5c0991ca7d9420c32e5d857c4cc35c3c56
BT_4N2_19 <cwalford3884@bths.edu>
2022-11-09 12:33:21 -0500

    Updated Design Document

---
+++ src/QuestSimulation.java
@@ -0,0 +1,25 @@
+public class QuestSimulation {
+    private String name;
+    private String role;
+
+    public QuestSimulation(String userName, String userRole) {
+        name = userName;
+        role = userRole;
+    }
+
+    public String introduction() {
+        String intro = "";
+        return intro;
+    }
+
+    public int wizardDamage() {
+        int damage = 0;
+        return damage;
+    }
+
+    public int knightDamgage() {
+        int damage = 0;
+        return damage;
+    }
+
+}
