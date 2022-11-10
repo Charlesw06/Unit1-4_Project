e54f2468b96bb20a143b2be7f93288cc3830fe38
BT_4N2_19 <cwalford3884@bths.edu>
2022-11-10 12:33:18 -0500

    Added new methods to QuestSimulation class and updated ProjectDesignDocument

--- src/QuestRunner.java
+++ src/QuestRunner.java
@@ -2,18 +2,35 @@ import java.util.Scanner;
 
 public class QuestRunner {
     public static void main(String[] args) {
+        System.out.println("-=| THE HUNT FOR THE EYE OF DAS |=-\n");
+
         Scanner s = new Scanner(System.in);
         System.out.print("What is the name of your character? ");
         String userName = s.nextLine();
 
-        Scanner n = new Scanner(System.in);
+        Scanner role = new Scanner(System.in);
         System.out.print("What is the role of your character (k = knight, w = wizard, b = barbarian)? ");
-        String userRole = n.nextLine();
+        String userRole = role.nextLine();
+
+        while (!(userRole.equals("k") || userRole.equals("w") || userRole.equals("b"))) {
+            System.out.print("Please input a valid role: ");
+            userRole = role.nextLine();
+        }
+
         System.out.println();
-
         QuestSimulation newQuest = new QuestSimulation(userName, userRole);
+        System.out.print(newQuest.introduction());
 
-        System.out.println(newQuest.introduction());
+        Scanner answerToQuest = new Scanner(System.in);
+        String questAccept = answerToQuest.nextLine();
+
+        while (!(questAccept.equals("y") || questAccept.equals("n"))) {
+            System.out.print("Please input a valid answer: ");
+            questAccept = answerToQuest.nextLine();
+        }
+
+        System.out.println();
+        System.out.println(newQuest.questInitiation(questAccept));
 
     }
 }
--- src/QuestSimulation.java
+++ src/QuestSimulation.java
@@ -8,10 +8,26 @@ public class QuestSimulation {
     }
 
     public String introduction() {
-        String intro = "The silence is deafening as you stand before the throne of Emperor Avalon. It was only a matter of time before you were caught by the emperor's forces.\nI don't blame you. Those royal pastries looked amazing. A whole minute passes before he offers you a deal. You can either except your punishment or go on a little quest for him. ";
-        return intro;
+        String intro = "The silence is deafening as you kneel before the throne of Emperor Avalon. A whole minute passes before\nhe offers you a deal. You can either except your punishment or go on a quest for him. He wants you to\nretrieve the Eye of Das, a sacred artifact that lets the user view anywhere in the world that they wish.";
+        String question = "Do you accept his offer (y = yes, n = no)? ";
+        return intro + "\n\n" + question;
     }
 
+    public String questInitiation(String userResponse) {
+        if (userResponse.equals("y")) {
+            return "The emperor grins. He tells you that a horse and supplies are already waiting for you at the gate.\nYour quest has begun.";
+        }
+        else {
+            return "The emperor glares at you. Before you are able to get up, you feel the hilt of a guard's sword hit\nyour head and everything goes black. When you wake up, you are restrained on a wagon traveling through\nthe woods. Turns out you didn't have a choice at all. Your quest has begun.";
+        }
+    }
+
+    public String chapter1() {
+        if (role.equals("k")) {
+            String obstacleText = "In front of you stands the dim cave entrance that leads to the dungeon. As you are\nworking up the courage to step inside, you hear crackling echoing from the opening. All of a sudden,\n a small green figure lunges at you from the darkness. You knock it away with your shield and draw your sword. As the goblin gets up, two more run out of the dungeon to face you. ";
+        }
+        return "";
+    }
 
     public int wizardDamage(int move) {
         int damage = 0;
